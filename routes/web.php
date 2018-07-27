448566a8b57b498b0e5a2cfae6d99ae2abf5bdf6
Kevin Lövgren <kevlov@jackpotjoygroup.com>
2018-07-27 23:13:48 +0200

    Added cache clear

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/clear-cache', function(){
+    Artisan::call('cache:clear');
+});
+
 /**
  * Decks
  */
