6bb094374e11e39d727808b94f943958132288e7
Kevin Lövgren <kevlov@jackpotjoygroup.com>
2018-07-27 08:23:04 +0200

    fixed function use

--- src/app/Http/Controllers/YugiController.php
+++ src/app/Http/Controllers/YugiController.php
@@ -11,7 +11,7 @@ class YugiController extends Controller
 
     function index(YugiRepository $yugi_repository)
     {
-        $names = yugi_repository();
+        $names = $yugi_repository->getDeckNames();
         return view('yugi_start', ['names' => $names]);
     }
 }
