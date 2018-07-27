a7a1721e7c8a2a83b46c3ed0bd5a949810486e48
Kevin Lövgren <kevlov@jackpotjoygroup.com>
2018-07-27 08:25:21 +0200

    learning blade

--- src/resources/views/yugi_start.blade.php
+++ src/resources/views/yugi_start.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Decks: {{ names }}
+                    Decks: {{ $names }}
                 </div>
 
             </div>
